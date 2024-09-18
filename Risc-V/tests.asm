199c3ebadb3cbc6fa8380be3495172a198a2fe63
Victor André De Moraes <victorandre957@gmail.com>
2024-09-18 10:35:31 -0300

    adjustments for rars to run tests.asm

--- Risc-V/tests.asm
+++ Risc-V/tests.asm
@@ -14,15 +14,16 @@
 
     slt x13, x6, x8
 
-    jal x14, 0x00000008
+    jal x14, br
     nop
 
+br:
     jalr x15, 12(x14)
     nop
 
-    ori x16, x6, 0XFF
-    and x17, x6, 0XF
-    xori x18, x6, 0XF0
+    ori x16, x6, 0x000000ff
+    andi x17, x6, 0x000000ff
+    xori x18, x6, 0x000000F0
 
     slti x19, x6, 100
     sltiu x20, x6, 100
@@ -35,24 +36,24 @@
     srli x25, x6, 2
     srai x26, x6, 2
 
-    beq x6, x9, 0x00000008
+    beq x6, x9, bt
     nop
-
-    blt x6, x9, 0x00000008
+bt:
+    blt x6, x9, bg
     nop
-
-    bge x8, x6, 0x00000008
+bg:
+    bge x8, x6, btu
     nop
-
-    bltu x6, x8, 0x00000008
+btu:
+    bltu x6, x8, bgeu
     nop
-
-    bgeu x8, x0 0x00000008
+bgeu:
+    bgeu x8, x0 bne
     nop
-
-    bne x8, x0 0x00000008
+bne:
+    bne x8, x0 lui
     nop
-
+lui:
     lui x9, 2
     sw x6, 0(x29)
     lw x30, 0(x29)
